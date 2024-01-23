739324ba25488f8f688fffd750c4b93e4ac34c0e
msravya02 <157043597+msravya02@users.noreply.github.com>
2024-01-23 11:39:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+let variable1=9;
+ const pi=3.14;
+ var myName="sravya"
+ let variable2=5;
+ console.log(variable1<=9 && variable2<6);
+ console.log(variable1<=9 || variable2<6);
+ console.log(!(variable1<=9 || variable2<6));
+ console.log(2**3);
+ console.log(myName);
+ 
